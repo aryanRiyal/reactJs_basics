921db083852666314c7551289e4151a8e445bb21
aryanRiyal <aryan13.riyal@gmail.com>
2024-11-20 18:17:39 +0530

    relocated all of the smaller projects to a single directory and migrated the npx builds to vite

--- 2.customReactAndTodoApp/todo_app/src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = function () {
-    return <button className='todo-button'>Add Todo</button>;
-};
-
-export default Button;
--- 2.customReactAndTodoApp/todo_app/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = function (props) {
-    return <h1 className='todo-header'>{props.title}</h1>;
-};
-
-export default Header;
